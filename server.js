ce87e7118258f280644b4789f1e5a890f2451b0b
OlivierCoue <olivier28.coue@gmail.com>
2017-12-15 02:05:33 +0100

    fix views path

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ let app = express();
 
 app.set('port', (process.env.PORT || 2016));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, './views'));
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
